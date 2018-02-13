3d8af58a189471082038264b82f75c183e2b5dc4
hwhh <hwhh@st-andrews.ac.uk>
2018-02-12 18:06:35 -0800

    cleaned the project up

--- website/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-
-from . import views
-
-app_name = 'website'
-urlpatterns = [
-    url('^$', views.Landing.as_view()),
-]
--- website/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views import generic
-
-
-# Create your views here.
-class Landing(generic.View):
-    def get(self, request):
-        return render(request, 'website/pages/landing.html')
