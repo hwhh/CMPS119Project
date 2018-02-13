7c4aae5606a2dcf06a09de19933bd3b866ba21cb
hwhh <hwhh@st-andrews.ac.uk>
2018-02-12 17:13:18 -0800

    added template files

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 
+
 # Create your views here.
 class Landing(generic.View):
     def get(self, request):
