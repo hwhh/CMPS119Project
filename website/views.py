e0d36a223efb3566588b959ad480e1c3a16e776d
Andrea Silvestroni <96andrea9642@gmail.com>
2018-02-12 18:37:50 -0800

    Removed unneeded files

--- website/views.py
+++ website/views.py
@@ -4,23 +4,3 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, '../templates/index.html', context)
-
-
-def about(request):
-    context = {}
-    return render(request, '../templates/about.html', context)
-
-
-def work(request):
-    context = {}
-    return render(request, '../templates/work.html', context)
-
-
-def blog(request):
-    context = {}
-    return render(request,'../templates/blog.html', context)
-
-
-def contact(request):
-    context = {}
-    return render(request, '../templates/contact.html', context)
