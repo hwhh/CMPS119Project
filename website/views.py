749213bf81227c91942ed2eb3b27967346a82eb5
hwhh <hwhh@st-andrews.ac.uk>
2018-02-25 19:35:23 -0800

    updated the models

--- website/views.py
+++ website/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     context = {}
     return render(request, '../templates/index.html', context)
+
+
+def contact(request):
+    context = {}
+    return render(request, '../templates/contact.html', context)
